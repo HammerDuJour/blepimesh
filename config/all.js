939029ee29d0bd1b62b9fbe6be103c014687ca43
Aaron Propst <aaron@aaronpropst.com>
2014-03-15 22:58:04 -0600

    needed to go loopback on my mac, becauseserver's perceived ip and the one claimed by client were not the same.

--- config/all.js
+++ config/all.js
@@ -1,7 +1,7 @@
 module.exports = {
 	webServerPort: 80,
 	socketServerPort: 8081,
-	//nicName: 'en0', //osx
+	//nicName: 'lo0', //osx
 	nicName: 'wlan0', //my pi's
 	serverReportInterval: 20000, //in millis.
 	clientMockSampleInterval: 30000 //in millis
